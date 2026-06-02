b637d22b3b51ffac1d7c7d5e56ee446dd493a49e
Arthur Cartana <arthurcartana@gmail.com>
2026-06-01 21:51:01 -0300

    calculadora

--- 26-06/assets/scripts/calculadora.js
+++ 26-06/assets/scripts/calculadora.js
@@ -1,61 +1,92 @@
-//---------------- Números ----------------------------
-const btn0 = document.getElementById("btn0");
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const btn6 = document.getElementById("btn6");
-const btn7 = document.getElementById("btn7");
-const btn8 = document.getElementById("btn8");
-const btn9 = document.getElementById("btn9");
-//---------------- Operações --------------------------
-const btnClear = document.getElementById("btnC");
-const btnVezes = document.getElementById("btn*");
-const btnDividir = document.getElementById("btn/");
-const btnMais = document.getElementById("btn+");
-const btnMenos = document.getElementById("btn-");
-const btnIgual = document.getElementById("btn=");
-
+const numeros = document.querySelectorAll(".numero");
+const opercaoes = document.querySelector(".operacao");
 const texto = document.querySelector("h1");
-
-const resultado = document.querySelector(".visor")
-
-
-const operadores = [
-    {name: btn0, type: "numero", value: "0"},
-    {name: btn1, type: "numero", value: "1"},
-    {name: btn2, value: "2"},
-    {name: btn3, value: "3"},
-    {name: btn4, value: "4"},
-    {name: btn5, value: "5"},
-    {name: btn6, value: "6"},
-    {name: btn7, value: "7"},
-    {name:btn8, value: "8"},
-    {name: btn9, value: "9"},
-    {name: btnClear, value: "clear"},
-    {name: btnVezes, value: "*"},
-    {name: btnDividir, value: "/"},
-    {name: btnMais, value: "+"},
-    {name: btnMenos, value: "-"},
-    {name: btnIgual , value: "="}
-
-]
-
-let valorAtual = ""
+const visor = document.querySelector(".item")
 
 
-operadores.forEach((botao) => {
-    console.log(botao)
+let primeiroNumero = "";
+let segundoNumero = "";
+let numeroVazio = false;
+let operacao = "";
+let valorAtual = "";
 
-    botao.name.addEventListener("click", () => {
-        resultado.innerHTML = botao.value
 
-        if(botao.type === "numeros") {
-            valorAtual += botao.value
-            resultado.innerHTML = valorAtual
-            console.log(valorAtual)
-
+numeros.forEach(botao => {
+    botao.addEventListener("click", () => {
+        if (!numeroVazio) {
+            primeiroNumero += botao.textContent
+            visor.textContent = primeiroNumero
+        } else {
+            segundoNumero += botao.textContent
+            visor.textContent = primeiroNumero + operacao + segundoNumero
         }
     })
+});
+
+document.getElementById("btn+").addEventListener("click", () => {
+    operacao = "+";
+    numeroVazio = true;
+})
+
+document.getElementById("btn-").addEventListener("click", () => {
+    operacao = "-";
+    numeroVazio = true;
+})
+
+document.getElementById("btn*").addEventListener("click", () => {
+    operacao = "*";
+    numeroVazio = true;
+})
+
+document.getElementById("btn/").addEventListener("click", () => {
+    operacao = "/";
+    numeroVazio = true;
+})
+
+document.getElementById("btnC").addEventListener("click", () => {
+    primeiroNumero = "";
+    segundoNumero = "";
+    numeroVazio = false;
+    operacao = "";
+    valorAtual = "";
+    visor.textContent = "0"
+})
+
+document.getElementById("btn=").addEventListener("click", () => {
+
+    const num1 = Number(primeiroNumero)
+    const num2 = Number(segundoNumero)
+    
+    let resultado;
+    switch(operador){
+        case "+":
+            
+            resultado = num1 + num2
+            break
+        case "-":
+            resultado = num1 - num2
+            break
+        case "*":
+            resultado = num1*num2
+            break
+        case "/":
+            if(num2 == 0){
+                resultado = "Erro"
+                
+            }else{
+                resultado = num1/num2
+            }
+            break
+
+            default:
+                resultado = "Erro"
+                
+}
+
+    visor.textContent = resultado
+
+    primeiroNumero = resultado.toString()
+    segundoNumero = ""
+    operador = ""
+    numeroVazio = false
 })
