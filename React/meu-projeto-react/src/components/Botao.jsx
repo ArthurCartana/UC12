f26373d344ef3d66385bef7591ec3de4a249f497
Arthur Cartana <arthurcartana@gmail.com>
2026-06-30 21:49:02 -0300

    react

--- React/meu-projeto-react/src/components/Botao.jsx
+++ React/meu-projeto-react/src/components/Botao.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Botao({text}) {
     return (
         // em JSX, não existe 'class' e sim 'className'
-        <button className="bg-indigo-600 px-2 py-2 rounded text-white hover:bg-indigo-800 transition-all duration-400">{text}</button>
+        <button className="bg-indigo-600 px-2 py-2 rounded text-white hover:bg-indigo-800 transition-all duration-400 cursor-pointer">{text}</button>
     )
 }
 
